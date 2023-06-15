00425ccf999e94c5965a841daff7f460faeb7cea
Jewell Niccolls <jewell.niccolls@digital.education.gov.uk>
2023-06-15 17:31:42 +0100

    updates to the routing

--- app/routes/triage.js
+++ app/routes/triage.js
@@ -3,18 +3,71 @@ const _ = require('lodash')
 
 module.exports = router => {
 
-  router.post('/triage/DfE-Sign-in-account', (req, res) => {
-    if(req.session.data['DfE-Sign-in-account'] == 'dsi') {
+  router.post('/triage/choose-signin', (req, res) => {
+    if(req.session.data['signin'] == 'dsi') {
       res.redirect('/triage/dfe-sign-in')
     } else {
-      res.redirect('/triage/what-type-of-organisation-are-you-buying-for')
+      res.redirect('/triage/what-type-school')
     }
   })
-  router.post('/triage/what-type-of-organisation-are-you-buying-for', (req, res) => {
-    if(req.session.data['what-type-of-organisation-are-you-buying-for'] == 'single') {
-      res.redirect('/triage/is-this-the-school-youre-buying-for')
+
+
+  router.post('/triage/what-type-school', (req, res) => {
+    if(req.session.data['type-organisation'] == 'single') {
+      res.redirect('/triage/search-for-your-school')
+    } else {
+      res.redirect('/triage/search-for-an-academy-trust-or-federation')
+    }
+  })
+
+  router.post('/triage/search-for-your-school', (req, res) => {
+    res.redirect('/triage/check-school')
+  })
+
+  router.post('/triage/search-for-an-academy-trust-or-federation', (req, res) => {
+    res.redirect('/triage/check-academy')
+  })
+
+  router.post('/triage/check-school', (req, res) => {
+    if(req.session.data['correct-school'] == 'yes') {
+      res.redirect('/triage/contact-details')
+    } else {
+      res.redirect('/triage/search-for-your-school')
+    }
+  })
+
+  router.post('/triage/check-academy', (req, res) => {
+    if(req.session.data['correct-academy'] == 'yes') {
+      res.redirect('/triage/select-schools-in-your-academy-trust-or-federation')
+    } else {
+      res.redirect('/triage/search-for-an-academy-trust-or-federation')
+    }
+  })
+
+ 
+  router.post('/triage/select-schools-in-your-academy-trust-or-federation', (req, res) => {
+    res.redirect('/triage/are-these-the-schools-youre-buying-for')
+  })
+
+  router.post('/triage/are-these-the-schools-youre-buying-for', (req, res) => {
+    if(req.session.data['correct-academy'] == 'yes') {
+      res.redirect('/triage/contact-details')
     } else {
       res.redirect('/triage/select-schools-in-your-academy-trust-or-federation')
     }
   })
+
+  router.post('/triage/access-needs', (req, res) => {
+    if(req.session.data['access-details'] == 'yes') {
+      res.redirect('/triage/check-your-answers')
+    } else {
+      res.redirect('/triage/check-your-answers-single-school')
+    }
+  })
+
+  router.post('/triage/is-there-anything-else-we-should-know', (req, res) => {
+    res.redirect('/triage/access-needs')
+  })
+  
 }
+
